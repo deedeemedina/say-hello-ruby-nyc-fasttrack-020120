5ae8ca899bc59b31a7fe47023d2b9f1c978613f3
deedee medina <deedee.medinacorp@gmail.com>
2020-02-04 17:39:26 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,8 @@
 # Build your say_hello method here
-def say_hello(name = "Hello Ruby programmer!")
-  puts "#{name}"
+def say_hello(name)
+  puts "Hello Kent Beck"
+end
+
+def say_hello(name="Ruby Programmer!")
+  puts ("Hello #{name}!")
 end
